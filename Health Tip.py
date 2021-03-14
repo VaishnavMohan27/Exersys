ddabb2e98586af7bc8a111d46be81867925de1f4
bradycardia-12 <77842981+bradycardia-12@users.noreply.github.com>
2021-03-14 13:40:10 +0530

    Update Health Tip.py

--- Health Tip.py
+++ Health Tip.py
@@ -11,7 +11,22 @@ q10='Minimize your sugar intake'
 q11='Don’t eat a lot of refined carbs!'
 q12='Check your blood pressure regularly'
 q13='Take antibiotics only as prescribed!'
-l1=[q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13]
+q14="Avoid alcohol"
+q15="Consume less sodium"
+q16="DO NOT SMOKE!"
+q17="Prevent mosquito bites"
+q18="Your mental health matters too"
+q19="Eat nuts"
+q20="Get 30 minutes of physical activity"
+q21="Do no skip breakfast"
+q22="Meditate"
+q23="Choose walking over travelling by a vehicle for short distances"
+q24="Give your eyes a rest"
+q25="Wash your hands!"
+q26="Eat vitamin supplements if you don't get them in your diet"
+q27="Get your sugar levels tested"
+q28="Make sure your haemoglobin levels don't drop"
+l1=[q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,q16,q17,q18,q19,q20,q21,q22,q23,q24,q25,q26,q27,q28]
 import random
 from tkinter import *
 gui=Tk()
@@ -22,5 +37,6 @@ def printter():
     var.set(random.choice(l1))
     w2=Label(gui,textvariable=var,fg='black',bg='white')
     w2.pack()
-bttn1=Button(gui,text="GET YOUR HEALTH TIP", fg='black', bg='white',command=printter)           
+    bttn1["state"]=DISABLED
+bttn1=Button(gui,text="GET YOUR HEALTH TIP OF THE DAY!", fg='black', bg='white',command=printter)           
 bttn1.pack()
